e04d3522faff966c3f4044947c1c7dee61718343
lwerre <49202932+lwerre@users.noreply.github.com>
2020-03-05 14:52:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+/*HW6a script file. this is where you define the content that will be revealed when the button is clicked on
+*/
+var s1 = {
+    /*define the variables*/
+    grade: "4",
+    title: "Accessing Information",
+    Description: "The student will be able to demonstrate the ability to access valid health information, products, and services to enhance health.",
+    CognitiveComplexity: "N/A",
+    Example: "Student knows where to look for scientifically accurate information regarding basic hygeine and diet.",
+    
+};
+/*recall variables to display onclick*/
+var se1 = "<b>grade: </b>" + s1.grade + 
+          "<br> <b>Title: </b>" + s1.title + 
+          "<br> <b>Description: </b>" + s1.Description + 
+          "<br> <b>CognitiveComplexity: </b>" + s1.CognitiveComplexity + 
+          "<br> <b>Example: </b>" + s1.Example + ""
+          
+
+var s2 = {
+/*define the variables*/
+    grade: "4",
+    title: "Health Literacy Responsible Behavior",
+    Description: "The student will be able to demonstrate the ability to use interpersonal-communication skills to enhance health and avoid or reduce health risks.",
+    CognitiveComplexity: "N/A",
+    Example: "Student knows where to look for scientifically accurate information regarding basic hygeine and diet.",
+            
+    };
+
+    /*recall variables to display onclick*/
+    var se2 = "<b>grade: </b>" + s2.grade + 
+    "<br> <b>Title: </b>" + s2.title + 
+    "<br> <b>Description: </b>" + s2.Description + 
+    "<br> <b>CognitiveComplexity: </b>" + s2.CognitiveComplexity + 
+    "<br> <b>Example: </b>" + s2.Example + ""
+                  
+
